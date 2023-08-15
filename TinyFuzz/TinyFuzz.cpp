62502ba2110a8972c0c17919afeec3b064da6e51
Michael Howard <mikehow@microsoft.com>
2023-08-14 21:48:32 -0500

    Added tighter fuzz range

--- TinyFuzz/TinyFuzz.cpp
+++ TinyFuzz/TinyFuzz.cpp
@@ -3,24 +3,38 @@
 #include <memory.h>
 #include <sal.h>
 
-#define MIN_BUFF_LEN 8
+// Written by: Michael Howard (mikehow@microsoft.com)
+// Microsoft Corporation
+// I have tried to keep this code as simple as possible.
+// It is not meant to be a good fuzzer, just a simple one.
+// And it makes little use of C++ idioms to make it easier to use. 
+// ie; no use of STL
 
 bool Fuzz(
-	_Inout_updates_bytes_(*pLen) char* pBuf, 
-	_Inout_						 unsigned int *pLen) { 
-
-	bool ok = true;
+	_Inout_updates_bytes_(*pLen) char* pBuf,
+	_Inout_						 size_t* pLen,
+	_In_range_(5, 100) 			 size_t fuzzPercent) noexcept {
 
 	// check for nulls
-	if (pBuf == NULL || pLen == NULL) {
+	if (pBuf == nullptr || pLen == nullptr) {
 		return false;
 	}
-	
+
+	constexpr size_t MIN_BUFF_LEN = 8;
+
 	// if data is too small to fuzz, return false
 	if (*pLen < MIN_BUFF_LEN) {
 		return false;
 	}
-	
+
+	if (fuzzPercent > 100) {
+		fuzzPercent = 10;
+	}
+
+	if (fuzzPercent < 5) {
+		fuzzPercent = 5;
+	}
+
 	// don't fuzz everything
 	if ((rand() % 100) > 25) {
 		return false;
@@ -30,43 +44,43 @@ bool Fuzz(
 	unsigned int start = rand() % *pLen;
 	unsigned int end = rand() % *pLen;
 	if (start > end) {
-		unsigned int tmp = start;
+		const unsigned int tmp = start;
 		start = end;
 		end = tmp;
 	}
 
 	// don't fuzz if the range is too small
-	if (end - start <= MIN_BUFF_LEN/2) {
+	if (end - start <= MIN_BUFF_LEN / 2) {
 		return false;
 	}
 
 	// how many loops through the fuzzer?
-	unsigned int iterations = 1 + rand() % 10;
+	const unsigned int iterations = 1 + rand() % 10;
 	for (unsigned int i = 0; i < iterations; i++) {
 
-		unsigned int skip = rand() % 10 > 7 ? 1 + rand() % 10 : 1;
-		unsigned int whichMutation = rand() % 8;
+		const unsigned int skip = rand() % 10 > 7 ? 1 + rand() % 10 : 1;
+		const unsigned int whichMutation = rand() % 8;
 		unsigned int j = 0;
 
 		switch (whichMutation) {
 			// set the range to a random byte
-			case 0: 
-				{
-					char byte = rand() % 256;
-					for (j = start; j < end; j += skip) {
-						pBuf[j] = byte;
-					}
+			case 0:
+			{
+				const char byte = rand() % 256;
+				for (j = start; j < end; j += skip) {
+					pBuf[j] = byte;
 				}
-				break;
+			}
+			break;
 
 			// write random bytes to the range
-			case 1: 
-				{
-					for (j = start; j < end; j += skip) {
-						pBuf[j] = rand() % 256;
-					}
+			case 1:
+			{
+				for (j = start; j < end; j += skip) {
+					pBuf[j] = rand() % 256;
 				}
-				break;
+			}
+			break;
 
 			// set upper bit
 			case 2:
@@ -83,36 +97,36 @@ bool Fuzz(
 				break;
 
 			// set the first zero-byte found to non-zero
-			case 4: 
-				{
-					for (j = start; j < end; j += skip) {
-						if (pBuf[j] == 0) {
-							pBuf[j] = rand() % 256;
-							break;
-						}
+			case 4:
+			{
+				for (j = start; j < end; j += skip) {
+					if (pBuf[j] == 0) {
+						pBuf[j] = rand() % 256;
+						break;
 					}
 				}
-				break;
+			}
+			break;
 
 			// insert interesting numbers
-			case 5: 
-				{
-					int interesting[] = { 0,1,7,8,9,15,16,17,31,32,33,63,64,65,127,128,129,191,192,193,223,224,225,239,240,241,247,248,249,253,254,255 };
-					for (j = start; j < end; j += skip) {
-						pBuf[j] = (char)interesting[rand() % (sizeof(interesting) / sizeof(int))];
-					}
+			case 5:
+			{
+				const int interestingNum[] = { 0,1,7,8,9,15,16,17,31,32,33,63,64,65,127,128,129,191,192,193,223,224,225,239,240,241,247,248,249,253,254,255 };
+				for (j = start; j < end; j += skip) {
+					pBuf[j] = (char)interestingNum[rand() % _countof(interestingNum)];
 				}
-				break;
+			}
+			break;
 
 			// interesting characters
-			case 6: 
-				{
-					char interesting2[] = { ':', ';', '<', '>', '\\', '/' };
-					for (j = start; j < end; j += skip) {
-						pBuf[j] = interesting2[rand() % (sizeof(interesting2) / sizeof(char))];
-					}
+			case 6:
+			{
+				const char interestingChar[] = { ':', ';', '<', '>', '\\', '/', '.'};
+				for (j = start; j < end; j += skip) {
+					pBuf[j] = interestingChar[rand() % _countof(interestingChar)];
 				}
-				break;
+			}
+			break;
 
 			// truncate
 			case 7:
@@ -124,26 +138,30 @@ bool Fuzz(
 		}
 	}
 
-	return ok;
+	return true;
 }
-
 
 int main()
 {
-	unsigned int count = 0;
+	size_t count = 0;
+	constexpr size_t fuzzPercent = 10;
+
 	while (1) {
 		char buf[201];
 		memset(buf, 'A', sizeof(buf));
 
-		unsigned int len = sizeof(buf);
-		bool fOk = Fuzz(buf, &len);
+		size_t len = sizeof(buf);
+		const bool fOk = Fuzz(buf, &len, fuzzPercent);
 
 		if (fOk) {
 			char buf2[201];
-			memcpy(buf2, buf, len);
+			memcpy_s(buf2, sizeof(buf2), buf, len);
 			buf2[len-1] = 0;
 
-			printf("\n[iter: %d] size=%d\n\t%s ", ++count, len, buf2);
+			printf("\n[iter: %zu] size=%zu\n\t%s ", 
+				++count, 
+				len, 
+				buf2);
 		}
 	}
 }
