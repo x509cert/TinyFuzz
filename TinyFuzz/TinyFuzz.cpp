9074b6d93ee2596e313d14e123992eb6e11fa045
Michael Howard <mikehow@microsoft.com>
2023-08-16 20:36:34 -0500

    minor edits

--- TinyFuzz/TinyFuzz.cpp
+++ TinyFuzz/TinyFuzz.cpp
@@ -45,102 +45,51 @@ class Fuzzer {
 		}
 
 		bool Fuzz(_Inout_updates_bytes_(*pLen)	char* pBuf,
-				  _Inout_						size_t* pLen) noexcept {
+			_Inout_						size_t* pLen) noexcept {
 
-<<<<<<< HEAD
+
 			// don't fuzz everything
 			if ((rand() % 100) > MIN_FUZZ_PERCENT) {
 				return false;
-=======
-	// if data is too small to fuzz, return false
-	if (*pLen < MIN_BUFF_LEN) {
-		return false;
-	}
 
-	if (fuzzPercent > 100) {
-		fuzzPercent = 10;
-	}
-
-	if (fuzzPercent < 5) {
-		fuzzPercent = 5;
-	}
-
-	// don't fuzz everything
-	if ((rand() % 100) > fuzzPercent) {
-		return false;
-	}
-
-	// get a random range to fuzz
-	unsigned int start = rand() % *pLen;
-	unsigned int end = rand() % *pLen;
-	if (start > end) {
-		const unsigned int tmp = start;
-		start = end;
-		end = tmp;
-	}
-
-	// don't fuzz if the range is too small
-	if (end - start <= MIN_BUFF_LEN / 2) {
-		return false;
-	}
-
-	// how many loops through the fuzzer?
-	const unsigned int iterations = 1 + rand() % 10;
-	for (unsigned int i = 0; i < iterations; i++) {
-
-		const unsigned int skip = rand() % 10 > 7 ? 1 + rand() % 10 : 1;
-		const unsigned int whichMutation = rand() % 8;
-		unsigned int j = 0;
-
-		switch (whichMutation) {
-			// set the range to a random byte
-			case 0:
-			{
-				const char byte = rand() % 256;
-				for (j = start; j < end; j += skip) {
-					pBuf[j] = byte;
+				// check for nulls
+				if (pBuf == nullptr || pLen == nullptr) {
+					return false;
 				}
->>>>>>> e017eab7ae687917fe2946179e92d3144487bd8e
-			}
 
-			// check for nulls
-			if (pBuf == nullptr || pLen == nullptr) {
-				return false;
-			}
+				// if data is too small to fuzz, return false
+				if (*pLen < MIN_BUFF_LEN) {
+					return false;
+				}
 
-			// if data is too small to fuzz, return false
-			if (*pLen < MIN_BUFF_LEN) {
-				return false;
-			}
+				// get a random range to fuzz
+				size_t start = rand() % *pLen;
+				size_t end = rand() % *pLen;
+				if (start > end) {
+					const size_t tmp = start;
+					start = end;
+					end = tmp;
+				}
 
-			// get a random range to fuzz
-			size_t start = rand() % *pLen;
-			size_t end = rand() % *pLen;
-			if (start > end) {
-				const size_t tmp = start;
-				start = end;
-				end = tmp;
-			}
+				// don't fuzz if the range is too small
+				if (end - start <= MIN_BUFF_LEN / 2) {
+					return false;
+				}
 
-			// don't fuzz if the range is too small
-			if (end - start <= MIN_BUFF_LEN / 2) {
-				return false;
-			}
+				// how many loops through the fuzzer?
+				// most of the, 10%, keep it at one iteration
+				const unsigned int iterations = rand() % 10 == 7
+					? 1
+					: 1 + rand() % 10;
 
-			// how many loops through the fuzzer?
-			// most of the, 10%, keep it at one iteration
-			const unsigned int iterations = rand() % 10 == 7 
-				? 1 
-				: 1 + rand() % 10;
+				for (unsigned int i = 0; i < iterations; i++) {
 
-			for (unsigned int i = 0; i < iterations; i++) {
+					const unsigned int skip = rand() % 10 > 7 ? 1 + rand() % 10 : 1;
+					const unsigned int whichMutation = rand() % 8;
+					unsigned int j = 0;
 
-				const unsigned int skip = rand() % 10 > 7 ? 1 + rand() % 10 : 1;
-				const unsigned int whichMutation = rand() % 8;
-				unsigned int j = 0;
-
-				switch (whichMutation) {
-					// set the range to a random byte
+					switch (whichMutation) {
+						// set the range to a random byte
 					case 0:
 					{
 						const char byte = rand() % 256;
@@ -166,34 +115,34 @@ class Fuzzer {
 						}
 						break;
 
-					// reset upper bit
+						// reset upper bit
 					case 3:
 						for (j = start; j < end; j += skip) {
 							pBuf[j] &= 0x7F;
 						}
 						break;
 
-					// set the first zero-byte found to non-zero
+						// set the first zero-byte found to non-zero
 					case 4:
-						{
-							for (j = start; j < end; j += skip) {
-								if (pBuf[j] == 0) {
-									pBuf[j] = rand() % 256;
-									break;
-								}
+					{
+						for (j = start; j < end; j += skip) {
+							if (pBuf[j] == 0) {
+								pBuf[j] = rand() % 256;
+								break;
 							}
 						}
-						break;
+					}
+					break;
 
 					// insert interesting numbers
 					case 5:
-						{
-							const int interestingNum[] = { 0,1,7,8,9,15,16,17,31,32,33,63,64,65,127,128,129,191,192,193,223,224,225,239,240,241,247,248,249,253,254,255 };
-							for (j = start; j < end; j += skip) {
-								pBuf[j] = (char)interestingNum[rand() % _countof(interestingNum)];
-							}
+					{
+						const int interestingNum[] = { 0,1,7,8,9,15,16,17,31,32,33,63,64,65,127,128,129,191,192,193,223,224,225,239,240,241,247,248,249,253,254,255 };
+						for (j = start; j < end; j += skip) {
+							pBuf[j] = (char)interestingNum[rand() % _countof(interestingNum)];
 						}
-						break;
+					}
+					break;
 
 					// interesting characters
 					case 6:
@@ -213,16 +162,16 @@ class Fuzzer {
 					default:
 						break;
 					}
+				}
+
+				return true;
+
 			}
-
-			return true;
-
 		}
 };
 
 
-int main()
-{
+int main() {
 	constexpr size_t fuzzPercent = 10;
 	
 	auto fuzz = std::make_unique<Fuzzer>(fuzzPercent);
